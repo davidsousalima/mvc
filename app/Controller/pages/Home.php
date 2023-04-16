5649740b12396cd74770d6bd58bdb77bfd144e99
David Sousa <99605426+davidsousalima@users.noreply.github.com>
2023-04-15 21:03:50 -0300

    Updates na home

--- app/Controller/pages/Home.php
+++ app/Controller/pages/Home.php
@@ -2,6 +2,12 @@
 	namespace App\Controller\Pages;
 
 	class Home{
-		
+		/**
+		 * Método responsável por retornar o conteudo (view) da nossa home 
+		 * @return string  
+		 */
+		public static function getHome(){
+			return 'Olá, Mundo!';
+		}
 	}
 ?>
